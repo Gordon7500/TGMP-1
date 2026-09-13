0b391ed6df2d58e6f9a6f56bb967479e3611404e
Gordon7500 <gordonhaley7500@gmail.com>
2026-09-13 18:05:40 +0530

    Update PlaybackService.kt

--- app/src/main/java/com/tuned/app/audio/PlaybackService.kt
+++ app/src/main/java/com/tuned/app/audio/PlaybackService.kt
@@ -54,7 +54,7 @@ class PlaybackService : Service() {
         super.onCreate()
         store = Store(applicationContext)
         effects = AudioEffectsController(store)
-        engine = AudioEngine(OutputDeviceRouter(applicationContext), applicationContext)
+        engine = AudioEngine(OutputDeviceRouter(applicationContext), applicationContext, effects.bandEqualizer)
         engine.onStateChanged = { s -> onEngineStateChanged(s) }
         engine.onProgress = { pos, dur ->
             _state.value = _state.value.copy(positionMs = pos, durationMs = dur)
